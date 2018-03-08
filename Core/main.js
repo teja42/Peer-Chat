754029f71e68a92e48664da3b304dde749926fb6
Shiva Teja <shivateja5050@outlook.com>
2018-03-08 22:10:29 +0530

    Init

---
+++ Core/main.js
@@ -0,0 +1,15 @@
+const electron = require("electron");
+const {app,BrowserWindow} = electron;
+
+let loadWindow, mainWindow;
+
+app.on("ready",()=>{
+   loadWindow = new BrowserWindow({
+      frame: false,
+      width: 700,
+      height:400
+   });
+   loadWindow.loadURL(`file://${__dirname}/../UI/index.html`);
+});
+
+app.on("window-all-close",()=>app.exit(0));
